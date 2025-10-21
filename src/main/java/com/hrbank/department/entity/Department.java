586b3f4954280fd127134150f6949766b2e240d8
권찬호 <rnjscksdud17@gmail.com>
2025-10-21 10:42:09 +0900

    Feat: 엔티티 구현
    엔티티 구현
    
    Resolves: #1

---
+++ src/main/java/com/hrbank/department/entity/Department.java
@@ -0,0 +1,43 @@
+package com.hrbank.department.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "department")
+public class Department {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "name", nullable = false, unique = true, length = 255)
+  private String name;
+
+  @Column(name = "description", nullable = false, length = 255)
+  private String description;
+
+  @Column(name = "established_at", nullable = false)
+  private LocalDate establishedAt;
+
+  @OneToMany(mappedBy = "department")
+  private List<Employee> employess = new ArrayList<>();
+
+}
