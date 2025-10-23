3e2b3b1de09541530d4b5be28b94eea09327c0f4
HOGUN00 <7080rjs@gmail.com>
2025-10-23 17:26:19 +0900

    Docs: 컨버터 주석 수정

--- src/main/java/com/hrbank/converter/GenericEnumConverter.java
+++ src/main/java/com/hrbank/converter/GenericEnumConverter.java
@@ -8,10 +8,6 @@ import org.springframework.core.convert.converter.Converter;
  1. Enum 클래스 정의: public abstract class Enum<E extends Enum<E>> implements Constable, Comparable<E>, Serializable
  2. converter 인터페이스 정의: public interface Converter<S, T>
  3. Enum.valueOf() : public static <T extends Enum<T>> T valueOf(Class<T> enumClass, String name)
- 4. 제너릭 와일드카드:
-  - <? extends T> : 와일드 카드의 상한 제한, T와 그 자손들만 가능
-  - <? super T> : 와일드 카드의 하한 제한, T와 그 조상들만 가능
-  - <?> : 제한 없음. 모든 타입 가능
  */
 @RequiredArgsConstructor
 public class GenericEnumConverter<T extends Enum<T>> implements Converter<String, T> {
