a9fcfb2a87db734508a3f9e7914f0b1360c75202
HOGUN00 <7080rjs@gmail.com>
2025-10-25 21:47:48 +0900

    feat: test위한 bean 추가

--- src/main/java/com/hrbank/backup/BackupScheduler.java
+++ src/main/java/com/hrbank/backup/BackupScheduler.java
@@ -13,10 +13,10 @@ public class BackupScheduler {
     private final BackupService backupService;
     private final BackupScheduleProperties properties; // cron 표현식을 외부 설정에서 가져오기 위애 필요함
 
-    @Scheduled(cron = "#{@backupScheduleProperties.cron}")
-    public void runBackupJob() {
-        log.info("자동 백업 실행 시작 (작업자=system)");
-        backupService.start("system");
-        log.info("자동 백업 완료.");
-    }
+//    @Scheduled(cron = "#{@backupScheduleProperties.cron}")
+//    public void runBackupJob() {
+//        log.info("자동 백업 실행 시작 (작업자=system)");
+//        backupService.start("system");
+//        log.info("자동 백업 완료.");
+//    }
 }
